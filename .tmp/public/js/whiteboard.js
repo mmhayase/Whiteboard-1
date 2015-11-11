a8710f4551dd6b442d991d43b11b2ee817bfc3fa
Evan <ewinelan@andrew.cmu.edu>
2015-11-11 14:15:43 -0500

    Added jquery file for creating questions

---
+++ .tmp/public/js/whiteboard.js
@@ -0,0 +1,15 @@
+$(function() {
+	$('#submit').click(function(e) {
+		e.preventDefault();
+		var question = $('#question').val();
+		// var photo = $('#')
+		console.log('here');
+		$.ajax({
+			url: 'question/create?text='+question,
+			type: 'PUT',
+			success: function(result) {
+				$('#exRequests').html(JSON.stringify(result));
+			}
+		})
+		return false;
+	})
