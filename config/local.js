247458e5765043d1ffdf2f226c0fe44d6d6c9bc3
ianpgo <ianpgo@gmail.com>
2015-12-17 19:54:17 -0500

    increased hook timeout time

--- config/local.js
+++ config/local.js
@@ -31,6 +31,7 @@ module.exports = {
 
 port: process.env.OPENSHIFT_NODEJS_PORT,
     host: process.env.OPENSHIFT_NODEJS_IP,
+    hookTimeout: 30000, //30 seconds
 
   /***************************************************************************
    * Your SSL certificate and key, if you want to be able to serve HTTP      *
